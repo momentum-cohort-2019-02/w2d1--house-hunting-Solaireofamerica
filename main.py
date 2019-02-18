a49c5f26a15c7827dec8c513cc7c400a53d59d3c
Solaireofamerica <dennisdinwiddie@gmail.com>
2019-02-18 15:30:56 -0500

    final update

--- main.py
+++ main.py
@@ -4,18 +4,18 @@ def month_calculator():
 
     down_payment = total_cost * .25
 
-    current_savings = 0
+    c_savings = 0
     annual_salary = float(input('What is your annual salary?'))
-    portion_saved = float(input('What % of your salary are you going to save each month?'))
+    portionsaved = float(input('What % of your salary are you going to save?'))
 
     r = .04
 
     i = 0
 
-    while current_savings < down_payment:
-        monthly_savings = ((annual_salary * portion_saved) / 12)
-        current_savings = (((current_savings * r / 12) + monthly_savings) + current_savings)
-        print(current_savings)
+    while c_savings < down_payment:
+        monthly_savings = ((annual_salary * portionsaved) / 12)
+        c_savings = (((c_savings * r / 12) + monthly_savings) + c_savings)
+        print(c_savings)
         i += 1
 
     print("It will take you " + str(i) + " months to save up enough money.")
